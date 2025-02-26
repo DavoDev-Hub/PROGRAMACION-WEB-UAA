3a81c43f0c2fcb1c083873aafb4644787a1b6dde
DavoDev <juanpajmz13@gmail.com>
2025-02-26 08:08:26 -0600

    DOM example

--- Ejemplos uso DOM-20250226/JS03.js
+++
@@ -1,9 +0,0 @@
-function encender() {
-    document.getElementById("foco").src=
-    "img/focoEncendido.gif"
-  }
-  
-  function apagar() {
-    document.getElementById('foco').src=
-    'img/focoApagado.gif'
-  }
---
+++ Ejemplos uso DOM-20250226/js/JS03.js
@@ -0,0 +1,7 @@
+function encender() {
+  document.getElementById("foco").src = "img/focoEncendido.gif";
+}
+
+function apagar() {
+  document.getElementById("foco").src = "img/focoApagado.gif";
+}
