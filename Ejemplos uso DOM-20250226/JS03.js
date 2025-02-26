efd65782ff3b3ca8de57f600de8ff628f8433c73
DavoDev <juanpajmz13@gmail.com>
2025-02-26 08:05:23 -0600

    ejemplos uso DOM

---
+++ Ejemplos uso DOM-20250226/JS03.js
@@ -0,0 +1,9 @@
+function encender() {
+    document.getElementById("foco").src=
+    "img/focoEncendido.gif"
+  }
+  
+  function apagar() {
+    document.getElementById('foco').src=
+    'img/focoApagado.gif'
+  }
